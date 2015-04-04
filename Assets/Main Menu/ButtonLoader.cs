d5b556905be4b16c0e46fc195e1fb80be39ea197
ryanvall <rav43@case.edu>
2015-04-04 17:54:35 -0400

    Main Menu stuff
    Initial main menu files finally in, still a lot of fixing to do

---
+++ Assets/Main Menu/ButtonLoader.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonLoader : MonoBehaviour {
+
+	public void ContinueGame()
+	{
+		//Application.LoadLevel (1);
+		Debug.Log ("continue game");
+		audio.Play ();
+	}
+
+	//load screen
+	public void BeginGame()
+	{
+		//Application.LoadLevel (2);
+		Debug.Log ("begin game");
+		audio.Play ();
+	}
+
+	public void LoadGame()
+	{
+		//Application.LoadLevel (3);
+		Debug.Log ("load game");
+		audio.Play ();
+	}
+
+	public void DisplayOptions()
+	{
+		//Application.LoadLevel (4);
+		Debug.Log ("display options");
+		audio.Play ();
+	}
+
+	public void ExitGame()
+	{
+		//Application.Quit ();
+		Debug.Log ("exit game");
+	}
+}
