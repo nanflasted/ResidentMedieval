6e258f47bf4a247fbd8296eb2588500faac8e531
Dyanngg <yxd155@case.edu>
2015-04-21 16:34:38 -0400

    Scene Transition Demo <-> nature <-> town

---
+++ Assets/Script/DemoGateInside.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoGateInside : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			StartCoroutine(ChangeLocation());
+		}
+	}
+	
+	public IEnumerator ChangeLocation() {
+		GameObject.FindGameObjectWithTag("ST").GetComponent<Fading>().currentDestination = "demogateoutside";
+		float fadeTime = GameObject.FindGameObjectWithTag("ST").GetComponent<Fading>().BeginFade(1);
+		yield return new WaitForSeconds(fadeTime);
+		Application.LoadLevel(2);	
+	}
+}
